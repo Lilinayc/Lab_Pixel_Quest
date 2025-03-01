441a8596890a4fa2149a4d86596597df8c2f1b71
Lilinayc <levimendoza742@gmail.com>
2025-03-01 11:15:48 -0500

    code

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+
+    string Helloworld = "hello world";
+
+    // int - integer/non-decimal
+    // float - decimal number
+
+    string hello = "hello ";
+    string world = "world";
+
+    float numberOne = 2;
+    float numberTwo = 4.25f;
+
+    // number_two
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        string sum = hello + world;
+        //Debug.Log(sum);
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        // float sum = numberOne + numberTwo;
 
+        numberOne = numberOne + numberTwo; 
+        // Debug.Log("sum: " + sum);
+        Debug.Log("increase: " + numberOne);
     }
 }
